35f7c2beaae27edca45c00387ea4767ee4331d38
dkhanh25 <daikhanh2005@gmail.com>
2024-08-29 11:05:58 +0700

    added num

--- course_info/src/App.jsx
+++ course_info/src/App.jsx
@@ -44,6 +44,7 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3 ={exercises3}/>
+      <Total num={exercises1} />
       <Total num={total} />
     </div>
   )
